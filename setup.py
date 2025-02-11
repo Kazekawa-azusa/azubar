3b1e280ce36e42b4e151c2a2957c9068afe6883a
Kazekawa-azusa <iamben1025@gmail.com>
2025-02-11 11:47:55 +0800

    v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='azubar',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     license="MIT",
     install_requires=[],
